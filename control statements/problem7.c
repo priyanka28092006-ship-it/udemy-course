2a4a8420287614c866889519dd1a0fb1fc39f86c
priyanka28092006-ship-it <priyanka.28092006@gmail.com>
2026-04-07 20:27:10 +0000

    problem 7

--- control statements/problem7.c
+++ control statements/problem7.c
@@ -1,24 +1,25 @@
 //armstrong number from 100 to 999
-
 #include <stdio.h>
 
 int main(){
-    int rem ,fab=0; 
+    int rem,fab,temp; 
 for(int i =100;i<=999;i++){
-    while (i!=0){
+    fab=0;
+    temp=i;
+    while (temp!=0){
 
-        rem = i%10;
-        fab+=rem*rem*rem;
-        i/=10;
+        rem = temp%10;
+        fab+=(rem*rem*rem);
+        temp/=10;
 
     }
    if(fab==i){
 
-    printf(" number = %d is armstrong \n",i);
+    printf(" number = %d ,is armstrong \n",i);
    }
-   else{
-    printf(" number = %d is not  armstrong \n",i);
-   }
+  else{
+      printf(" number = %d ,is not armstrong \n",i);
+  }
 }
 
 }
